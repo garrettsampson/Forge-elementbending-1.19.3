136d2602d1dce40b6e31d2836fca54e080f354e5
garrettsampson <adr3nlin3ninja@gmail.com>
2023-02-02 04:39:25 -0500

    Initial commit

---
+++ src/main/java/net/garrett/elementbending/elementbending.java
@@ -0,0 +1,43 @@
+package net.garrett.elementbending;
+
+import com.mojang.logging.LogUtils;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.CreativeModeTabEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(elementbending.MOD_ID)
+public class elementbending {
+    public static final String MOD_ID = "elementbending";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public elementbending()
+    {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        modEventBus.addListener(this::commonSetup);
+
+        MinecraftForge.EVENT_BUS.register(this);
+
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+
+    }
+
+        // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
+        @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+        public static class ClientModEvents {
+            @SubscribeEvent
+            public static void onClientSetup(FMLClientSetupEvent event) {
+            }
+        }
+    }
